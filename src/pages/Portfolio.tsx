4e75cb91609bd9e0fe224584dab3f70ab92f0860
felipe-AGomes <falmeidagomes13@gmail.com>
2023-08-02 09:44:15 -0300

    add digital signature

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/object-curly-spacing */
+import React, { useEffect } from 'react';
 import AboutPage from '../components/AboutPage';
 import HomePage from '../components/HomePage';
 import ProjectPage from '../components/ProjectPage';
 import SkillPage from '../components/SkillPage';
+import { generateDigitalSignature } from 'assinatura-digital';
 
 function Portfolio() {
+	useEffect(() => {
+		console.clear();
+		const transformedSignature = generateDigitalSignature(
+			'felipe-dev',
+			'falmeidagomes13@gmail.com',
+			'Bem-vindo ao meu site! Sinta-se à vontade para explorar e desenvolver com paixão!',
+		);
+
+		console.log(...transformedSignature);
+	}, []);
+
 	return (
 		<main>
 			<HomePage />
